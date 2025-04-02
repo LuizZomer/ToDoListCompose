cd97ddfdb35a18ba5618959098f296127c725ed1
luiz.282422 <lipezomer@gmail.com>
2025-04-02 20:56:47 -0300

    teste

---
+++ app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
@@ -0,0 +1,37 @@
+package br.edu.satc.todolistcompose
+
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import androidx.room.Update
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "complete") val complete: Boolean
+)
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM task")
+    fun getAll(): List<Task>
+
+    @Insert
+    fun insertAll(vararg task: Task)
+
+    @Delete
+    fun delete(user: Task)
+}
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+}
